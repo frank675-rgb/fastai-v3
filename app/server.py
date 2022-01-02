c9a95fb9a799f05f4aa6d6ba7128e6c426a359fb
frank675-rgb <79220584+frank675-rgb@users.noreply.github.com>
2022-01-02 16:44:51 +0000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'http://ptak.felk.cvut.cz/plants/DanishFungiDataset/checkpoints/DF20/DF20-ResNet50_best_accuracy.pth'
+export_file_name = 'DF20-ResNet50_best_accuracy.pth'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
